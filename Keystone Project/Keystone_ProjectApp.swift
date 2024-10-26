bda27857aad1b975cfc2fd95a5d5236517df4d7f
Daniel Eybelman <deybelman@gmail.com>
2024-10-25 20:43:28 -0700

    Initial Commit

---
+++ Keystone Project/Keystone_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Keystone_ProjectApp.swift
+//  Keystone Project
+//
+//  Created by Daniel Eybelman on 10/25/24.
+//
+
+import SwiftUI
+
+@main
+struct Keystone_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Keystone ProjectTests/Keystone_ProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  Keystone_ProjectTests.swift
+//  Keystone ProjectTests
+//
+//  Created by Daniel Eybelman on 10/25/24.
+//
+
+import Testing
+@testable import Keystone_Project
+
+struct Keystone_ProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
